5922521757429394fd9e55a74c894c65379d6376
bimapriaaditya <bpabima@gmail.com>
2024-03-14 14:05:51 +0700

    add title page each function app

--- app/Controllers/Webview.php
+++ app/Controllers/Webview.php
@@ -6,42 +6,72 @@ class Webview extends BaseController
 {
     public function index()
     {
-        return view('index');
+        $data = [
+            "title" => "index"
+        ];
+        return view('index', $data);
     }
     public function data_pesanan()
     {
-        return view('data_pemesan');
+        $data = [
+            "title" => "data_pesanan"
+        ];
+        return view('data_pemesan', $data);
     }
     public function pengecoran_logam()
     {
-        return view('pengecoran_logam');
+        $data = [
+            "title" => "pengecoran_logam"
+        ];
+        return view('pengecoran_logam', $data);
     }
     public function pengujian_material()
     {
-        return view('pengujian_material');
+        $data = [
+            "title" => "pengujian_material"
+        ];
+        return view('pengujian_material', $data);
     }
     public function form_peleburan()
     {
-        return view('form_peleburan');
+        $data = [
+            "title" => "form_peleburan"
+        ];
+        return view('form_peleburan', $data);
     }
     public function form_qc()
     {
-        return view('form_qc');
+        $data = [
+            "title" => "form_qc"
+        ];
+        return view('form_qc', $data);
     }
     public function order_logistik()
     {
-        return view('order_logistik');
+        $data = [
+            "title" => "order_logistik"
+        ];
+        return view('order_logistik', $data);
     }
     public function project_selesai()
     {
-        return view('project_selesai');
+        $data = [
+            "title" => "project_selesai"
+        ];
+        return view('project_selesai', $data);
     }
     public function data_benda()
     {
-        return view('data_benda');
+        $data = [
+            "title" => "data_benda"
+        ];
+        return view('data_benda', $data);
     }
     public function data_bahan()
     {
-        return view('data_bahan');
+        $data = [
+            "title" => "data_bahan"
+        ];
+        return view('data_bahan', $data);
     }
 }
